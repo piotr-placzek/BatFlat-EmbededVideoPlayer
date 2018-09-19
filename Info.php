ca559969fd578faa85209c8f762627062bf254c2
Piotr Płaczek <piotr.placzek.91@gmail.com>
2018-09-19 07:28:28 +0200

    Update Info.php

--- Info.php
+++ Info.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* BatFlat - FilesToDownload Module by pplaczek.
-* Allows you to add to the page or post a list of files ready to be downloaded directly from the server.
+* BatFlat - EmbededVideoPlayer Module by pplaczek.
+* Allows you to upload a video clip and play it using the html5 player.
 *
 * @author       Piotr Płaczek <piotr@pplaczek.pl>
 * @copyright    2018 Piotr Płaczek <p.dev>
@@ -10,32 +10,30 @@
 */
 
 return [
-    'name'          =>  $core->lang['filestodownload']['module_name'],
-    'description'   =>  $core->lang['filestodownload']['module_desc'],
+    'name'          =>  $core->lang['embededvideoplayer']['module_name'],
+    'description'   =>  $core->lang['embededvideoplayer']['module_desc'],
     'author'        =>  'p.dev',
-    'version'       =>  '1.1',
+    'version'       =>  '1.0',
     'compatibility'    =>    '1.3.*',                                // Compatibility with Batflat version
-    'icon'          =>  'download',                                 // Icon from http://fontawesome.io/icons/
+    'icon'          =>  'film',                                 // Icon from http://fontawesome.io/icons/
 
     'install'       =>  function () use ($core) {
-        $core->db()->pdo()->exec("CREATE TABLE IF NOT EXISTS `pdev_ftd` (
+        $core->db()->pdo()->exec("CREATE TABLE IF NOT EXISTS `pdev_evp` (
             `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-            `icon` text NOT NULL,
             `name` text NOT NULL,
             `slug` text NOT NULL,
-            `size` integer NOT NULL DEFAULT 0,
             `file` text NOT NULL,
             `path` text NOT NULL
         )");
 
-        if (!file_exists(UPLOADS.'/pdev_ftd')) {
-            mkdir(UPLOADS.'/pdev_ftd', 0755, true);
+        if (!file_exists(UPLOADS.'/pdev_evp')) {
+            mkdir(UPLOADS.'/pdev_evp', 0755, true);
         }
     },
     'uninstall'     =>  function () use ($core) {
         // If you uncomment line bellow then you lost all data but not files
-        $core->db()->pdo()->exec("DROP TABLE `pdev_ftd`");
+        $core->db()->pdo()->exec("DROP TABLE `pdev_evp`");
         // If you uncomment block bellow then you lost all files but not data
-        deleteDir(UPLOADS.'/pdev_ftd');
+        deleteDir(UPLOADS.'/pdev_evp');
     }
 ];
